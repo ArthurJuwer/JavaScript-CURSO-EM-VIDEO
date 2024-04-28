5bbc198b3d5b9f5deb930b3c762ae6c58537461f
ArthurJuwer <arthur.juwer99@gmail.com>
2024-04-28 20:53:53 -0300

    Aula 14, Laço de repeticão FOR

---
+++ AULA-14/ex015.js
@@ -0,0 +1,23 @@
+// LAÇO DE REPETIÇÃO FOR
+
+// jeito mais pratico ->
+for(let i = 1; i <= 3; i++){
+    // inicio; teste; incremento
+    console.log(`FOR -> Passo ${i}`)
+}
+
+// jeito mais demorado ->
+/* {
+    let i = 1
+    while (i <= 3){
+        console.log(`WHILE -> Passo ${i}`)
+        i++
+    }
+} */
+
+// teste de depuração (DEBUG CONSOLE)
+console.log("Vai começar...")
+for(let i = 0; i <= 3; i++){
+    console.log(i)
+}
+console.log(i)
