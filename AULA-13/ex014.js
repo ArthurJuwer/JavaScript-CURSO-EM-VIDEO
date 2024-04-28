f68a1d349caeceb04a4319d52c3bfcabf862a524
ArthurJuwer <arthur.juwer99@gmail.com>
2024-04-28 20:35:44 -0300

    Exercicio 14, laços de repeticao com teste logico no inicio e final { while, do while }

---
+++ AULA-13/ex014.js
@@ -0,0 +1,31 @@
+
+//  teste logico inicio ->
+
+var contador = 1
+while (contador <= 8){
+    console.log(`Passo ${contador}`)
+    contador++
+}
+
+// teste logico final ->
+
+contador = 1
+do{
+    console.log(`Contador esta com o valor ${contador}`)
+    contador++
+} while (contador <= 1)
+
+
+
+//  mesma funcionalidade só que mais complicada e menos pratica ->
+
+/*
+console.log("Olá")
+console.log("Olá")
+console.log("Olá")
+console.log("Olá")
+console.log("Olá")
+console.log("Olá")
+console.log("Olá")
+console.log("Olá")
+*/
